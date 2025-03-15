a72c576fef8072c7ddcad782f577614cdf4b212d
downtomarsguy <theonesilverest@gmail.com>
2025-03-15 18:44:40 +0530

    Counter Class Created; generate_durations Function Added

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 use rand::Rng;
 
 // customer class
-struct Customer{
+struct Customer {
     need: String,
 }
 
@@ -14,9 +14,41 @@ impl Customer {
     }
 
     fn seed(&mut self) {
-        let tasks = vec!["Process Check".to_string(), "Check Balance".to_string(), "Open Account".to_string(), "Deposit Money".to_string()];
+        let tasks = vec![
+            "Process Check".to_string(),
+            "Check Balance".to_string(),
+            "Open Account".to_string(),
+            "Deposit Money".to_string(),
+        ];
         let mut rng = rand::rng();
-        self.need = tasks[rng.random_range(0..=tasks.len()-1)].to_string()
+        self.need = tasks[rng.random_range(0..=tasks.len() - 1)].to_string();
+    }
+}
+
+// counter classes
+struct Counter {
+    process_check: u8,
+    check_balance: u8,
+    open_account: u8,
+    deposit_money: u8,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter {
+            process_check: 0,
+            check_balance: 0,
+            open_account: 0,
+            deposit_money: 0,
+        }
+    }
+
+    fn generate_durations(&mut self) {
+        let mut rng = rand::rng();
+        self.process_check = rng.random_range(1..=10);
+        self.check_balance = rng.random_range(1..=10);
+        self.open_account = rng.random_range(1..=10);
+        self.deposit_money = rng.random_range(1..=10);
     }
 }
 
@@ -26,5 +58,13 @@ fn main() {
 
     customer.seed();
 
+    let mut counter = Counter::new();
+
+    counter.generate_durations();
+
     println!("{}", customer.need);
+    println!("{}", counter.process_check);
+    println!("{}", counter.check_balance);
+    println!("{}", counter.open_account);
+    println!("{}", counter.deposit_money);
 }
