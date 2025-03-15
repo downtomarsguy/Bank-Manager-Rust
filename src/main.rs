4eb752e31b98e30481d19843ff2fd02733045b80
downtomarsguy <theonesilverest@gmail.com>
2025-03-15 18:28:58 +0530

    Customer Class Created; seed Function Added

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
+// imports
+use rand::Rng;
+
+// customer class
+struct Customer{
+    need: String,
+}
+
+impl Customer {
+    fn new() -> Customer {
+        Customer {
+            need: String::new(),
+        }
+    }
+
+    fn seed(&mut self) {
+        let tasks = vec!["Process Check".to_string(), "Check Balance".to_string(), "Open Account".to_string(), "Deposit Money".to_string()];
+        let mut rng = rand::rng();
+        self.need = tasks[rng.random_range(0..=tasks.len()-1)].to_string()
+    }
+}
+
+// main function
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    
-    println!("The value of x is: {x}");
+    let mut customer = Customer::new();
+
+    customer.seed();
+
+    println!("{}", customer.need);
 }
